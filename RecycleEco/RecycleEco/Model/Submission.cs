39beb9c48ce31874ba7b3a54b9d0990ccb2d628e
serenarekha <serenarekha@rocketmail.com>
2020-03-22 22:54:13 +0800

    Create Record Submission XAML, CS
    Front end and back end done, havent tested

---
+++ RecycleEco/RecycleEco/Model/Submission.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecycleEco.Model
+{
+    class Submission
+    {
+        public string SubmissionID { get; internal set; }
+        public string Weight { get; internal set; }
+        public string Date { get; internal set; }
+    }
+}
---
+++ RecycleEco/RecycleEco/ViewModel/SubmissionVM.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using RecycleEco.Model;
+using RecycleEco.Utilities;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace RecycleEco.ViewModel
+{
+    class SubmissionVM : INotifyPropertyChanged
+    {
+        //create Collector object
+        public static Submission Submit{ get; set; }
+
+        //check if record was succesffuly create 
+        private bool createSubmit;
+        public bool CreateSubmit
+        {
+            get
+            {
+                return createSubmit;
+            }
+            set
+            {
+                createSubmit = value;
+                OnPropertyChanged();
+            }
+        }
+
+         // check if record was successfully submitted
+        private string recordSubmitStatus;
+        public string RecordSubmitStatus
+        {
+            get
+            {
+                return recordSubmitStatus;
+            }
+            set
+            {
+                recordSubmitStatus = value;
+                OnPropertyChanged();
+            }
+        }
+
+        //variables//////////////////////////////////////////////////////////////////////////////
+        public string SubmissionID
+        {
+            get { return Submit.SubmissionID; }
+            set
+            {
+                Submit.SubmissionID = value;
+                CreateSubmit = CheckFields(); 
+                OnPropertyChanged();
+            }
+        }
+        public string Weight
+        {
+            get { return Submit.Weight; }
+            set
+            {
+                Submit.Weight = value;
+                CreateSubmit = CheckFields();
+                OnPropertyChanged();
+            }
+        }
+
+        public string Date
+        {
+            get { return Submit.SubmissionID; }
+            set
+            {
+                Submit.SubmissionID = value;
+                CreateSubmit = CheckFields();
+                OnPropertyChanged();
+            }
+        }
+
+
+        // Interfaces///////////////////////////////////////////////////////////////////////////////
+        public ICommand submitCreated { get; set; }
+
+        // Important methods////////////////////////////////////////////////////////////////////////
+
+        private bool CheckFields()
+        {
+            bool result = !string.IsNullOrWhiteSpace(Submit.SubmissionID)&&
+                          !string.IsNullOrWhiteSpace(Submit.Weight) &&
+                          !string.IsNullOrWhiteSpace(Submit.Date);
+            return result;
+        }
+
+
+        private void OnPropertyChanged()
+        {
+            throw new NotImplementedException();
+        }
+
+        //this method is important to save data and later can be used to update if necessary
+        public event PropertyChangedEventHandler PropertyChanged;
+
+    }
+}
