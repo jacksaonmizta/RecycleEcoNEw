1eca3bc74a06d7a052f5c2db057e7b89f993fd16
serenarekha <serenarekha@rocketmail.com>
2020-04-08 16:05:25 +0800

    add Material to Collector done

--- RecycleEco/RecycleEco/ViewModel/CollectorMaterialVM.cs
+++ RecycleEco/RecycleEco/ViewModel/CollectorMaterialVM.cs
@@ -13,58 +13,74 @@ namespace RecycleEco.ViewModel
 {
     class CollectorMaterialVM : INotifyPropertyChanged
 
-	{
-		public static Collector collector { get; set; }
-		public static Material material { get; set; }
+    {
+        private Material chooseMaterial;
 
-		private object selectedItem;
+        public Material ChooseMaterial
+        {
+            get
+            {
+                return chooseMaterial;
+            }
+            set
+            {
+                chooseMaterial = value;
+                if (chooseMaterial != null)
+                    AddCollectorMaterial.Execute(chooseMaterial);
+                chooseMaterial = null;
+                OnPropertyChanged();
+            }
+        }
 
-		public object SelectedItem
-		{
-			get
-			{
-				return selectedItem;
-			}
-			set
-			{
-				selectedItem = value;
-				if (selectedItem == null)
-				{
-					OnPropertyChanged();
-				}
-				OpenCollectorMaterialView.Execute(selectedItem);
-				selectedItem = null;
-				OnPropertyChanged();
-			}
-		}
+        private ObservableCollection<Material> collectorMaterialList;
 
-		public ObservableCollection<Material> CollectorMaterialList { get; set; }
-		public ICommand OpenCollectorMaterialView { get; set; }
+        public ObservableCollection<Material> CollectorMaterialList
+        {
+            get { return collectorMaterialList; }
+            set
+            {
+                collectorMaterialList = value;
+                OnPropertyChanged();
+            }
+        }
 
 
-		public CollectorMaterialVM()
-		{
-			material = new Material();
-			
-			OpenCollectorMaterialView = new Command(OpenCollectorMaterialExecute);
-			CollectorMaterialList = new ObservableCollection<Material>();
-			GetAllMaterials();
-		}
+        public ICommand AddCollectorMaterial { get; set; }
+        public CollectorMaterialVM()
+        {
+            collectorMaterialList = new ObservableCollection<Material>();
+            GetAllMaterials();
+            AddCollectorMaterial = new Command<Material>(OpenCollectorMaterialExecute) ; ;
+        }
 
-		private async void GetAllMaterials()
-		{
-			CollectorMaterialList = await MaterialAuth.GetAllMaterials();
-		}
+        private async void GetAllMaterials()
+        {
+            CollectorMaterialList = await MaterialAuth.GetMaterialsById(CollectorVM.Collector.MaterialCollection);
+        }
+        private async void OpenCollectorMaterialExecute(Material material)
+        {
+            Collector collector = CollectorVM.Collector;
+            if (collector.MaterialCollection == null)
+                collector.MaterialCollection = new List<string>();
+            collector.MaterialCollection.Add(material.MaterialID);
+            await CollectorAuth.UpdateCollector(collector);
 
-		private  void OpenCollectorMaterialExecute(object obj)
-		{
-			collector.MaterialCollection.Add((string)selectedItem);
-		}
 
-		public event PropertyChangedEventHandler PropertyChanged;
-		private void OnPropertyChanged([CallerMemberName] string propertyName = "")
-		{
-			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-		}
-	}
+            if (material.CollectorList == null)
+                material.CollectorList = new List<string>();
+            material.CollectorList.Add(collector.Username);
+            await MaterialAuth.UpdateMaterial(material);
+            CollectorMaterialList.Remove(material);
+
+            await Application.Current.MainPage.DisplayAlert("Materials", "Material " + material.MaterialName + " is successfully added into collection.", "OK");
+            //await Application.Current.MainPage.Navigation.PopAsync();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
 }
+
--- RecycleEco/RecycleEco/ViewModel/MaterialListVM.cs
+++ RecycleEco/RecycleEco/ViewModel/MaterialListVM.cs
@@ -54,6 +54,21 @@ namespace RecycleEco.ViewModel
 				new Views.RecyclerChooseCollector());
 		}
 
+		private ObservableCollection<Material> availableMaterialList;
+
+		public ObservableCollection<Material> AvailableMaterialList
+		{
+			get { return availableMaterialList; }
+			set
+			{
+				availableMaterialList = value;
+				OnPropertyChanged();
+			}
+		}
+
+
+
+
 		private async void GetAllMaterials()
 		{
 			MaterialList = await MaterialAuth.GetMaterials();
